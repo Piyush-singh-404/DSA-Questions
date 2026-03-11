b1286a8033b9e5fc054dfaabd66f8147602e2216
Piyush Singh <ps2615970@gmail.com>
2026-03-11 13:04:04 +0530

    Create Question9.c

---
+++ Question9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1, mid;
+    while(low <= high) {
+        mid = (low + high) / 2;
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 4;
+    int index = binarySearch(arr, n, target);
+    printf("%d", index);
+    return 0;
+}
