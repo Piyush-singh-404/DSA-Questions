d4a9bb853914a019c8eabf0d758452262fe93b2f
Piyush Singh <ps2615970@gmail.com>
2026-03-11 12:56:37 +0530

    Create Question5.c

---
+++ Question5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int tyres(int cars, int bikes)
+{
+    return (cars * 4) + (bikes * 2);
+}
+
+int main()
+{
+    int n, cars, bikes, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d %d", &cars, &bikes);
+        printf("%d\n", tyres(cars, bikes));
+    }
+
+    return 0;
+}
